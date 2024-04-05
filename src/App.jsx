2b875ca11ca018482afed617c7a08ddb8c3e1330
projectmiluju <dnjsdyd0712@naver.com>
2024-04-05 19:19:46 +0900

    propagation 이벤트 전파 방지

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,19 @@ export default function App() {
   return (
     <>
       <BasicButton onClick={buttonCliked}>클릭해주세요</BasicButton>
+
+      <hr/>
+
+      <div className="Toolbar" onClick={() => alert('you clicked on the toolbar')}>
+        <button onClick={e => {
+            e.stopPropagation();
+            alert('Playing!')
+          }}>Play Movie</button>
+        <button onClick={e => {
+            e.stopPropagation()
+            alert('Uploading!')
+          }}>Uploading Image</button>
+      </div>
     </>
   );
 }
